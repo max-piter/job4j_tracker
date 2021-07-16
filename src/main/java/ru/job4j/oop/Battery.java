c2cd25cad36a69883be65e1bb1eac72acba5a46c
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-16 19:21:23 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(25);
+        Battery second = new Battery(50);
+        first.exchange(second);
+        System.out.println();
+
+
+    }
+}
