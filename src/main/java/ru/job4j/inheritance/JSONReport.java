01dd622f877a21bf229ddfd15d4be7c92d22fdda
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-18 16:08:45 +0300

    4. Переопределение [#174890 #178692]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -5,6 +5,6 @@ public class JSONReport extends TextReport {
     public String generate(String name, String body) {
         return '{' + System.lineSeparator() + name + " \"name\"" + ','
                 + System.lineSeparator() + body + " \"body\""
-                + System.lineSeparator() + "}";
+                + System.lineSeparator() + '}';
     }
 }
