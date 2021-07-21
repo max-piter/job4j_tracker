0cd00063c0418ad6640725cacd12f69acf9a7874
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-21 17:13:50 +0300

    7. Шаблон проектирования - Стратегия [#786 #180110]

--- src/main/java/ru/job4j/strategy/Triangle.java
+++ src/main/java/ru/job4j/strategy/Triangle.java
@@ -8,9 +8,5 @@ public class Triangle implements  Shape {
                     + " /   \\ " + ln
                     + "/_____\\" + ln;
         }
-
-        public static void main(String[] args) {
-            System.out.println(new Triangle().draw());
-        }
     }
 
