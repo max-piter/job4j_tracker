70a35972e3fab4435170a50c116f9b1b2c0d3be4
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-30 22:04:26 +0300

    2. Преобразования List в Map. [#10093]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -3,6 +3,7 @@ package ru.job4j.collection;
 import java.util.HashMap;
 
 public class UsageMap {
+
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("butcher@mail.ru", "Korovkin Max Vladimir");
@@ -12,5 +13,6 @@ public class UsageMap {
             String value = map.get(key);
             System.out.println(key + " = " + value);
         }
+
     }
 }
