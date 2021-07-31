f390982cf7d86446971d86b1947e59c2b0237560
max-piter <61771015+max-piter@users.noreply.github.com>
2021-08-01 00:36:27 +0300

    ConvertList

--- src/main/java/ru/job4j/collection/ConvertList.java
+++ src/main/java/ru/job4j/collection/ConvertList.java
@@ -1,6 +1,8 @@
 package ru.job4j.collection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ConvertList {
@@ -12,5 +14,16 @@ public class ConvertList {
             }
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        Integer first = 1;
+        Integer second = 2;
+        int rsl = first.compareTo(second);
+        System.out.println(rsl);
+        List<Integer> list = Arrays.asList(5, 3, 4, 1, 2);
+        System.out.println(list);
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list);
     }
 }
