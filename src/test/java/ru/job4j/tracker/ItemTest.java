24d33e17e98f06be12320d6c1cba176513ae014d
max-piter <61771015+max-piter@users.noreply.github.com>
2021-08-01 12:30:56 +0300

    0. Сортировка [#10096 #184175]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -1,14 +1,9 @@
 package ru.job4j.tracker;
 
-import junit.framework.TestCase;
 import org.junit.Test;
-
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class ItemTest {
 
