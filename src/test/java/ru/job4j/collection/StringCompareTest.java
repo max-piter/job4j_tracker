ca4edd0b6e266f7bdcf38b7cabdae9d43464fe0e
max-piter <61771015+max-piter@users.noreply.github.com>
2021-08-03 15:19:00 +0300

    исправления

--- src/test/java/ru/job4j/collection/StringCompareTest.java
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -19,7 +19,7 @@ public class StringCompareTest {
     }
 
     @Test
-    public void whenLeftLessThanRightResultShouldBeNegative () {
+    public void whenLeftLessThanRightResultShouldBeNegative() {
         StringCompare compare = new StringCompare();
         int rst = compare.compare(
                 "Ivanov",
