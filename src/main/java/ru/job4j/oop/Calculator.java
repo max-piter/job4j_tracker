0933245832c308119ea5106e892faa60843e816e
max-piter <61771015+max-piter@users.noreply.github.com>
2021-11-13 12:21:57 +0300

    1.9. Статические и не статические методы [#504806]

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,36 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int multiply(int y) {
+        return y * x;
+    }
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int y) {
+        return y - x;
+    }
+
+    public int divide(int y) {
+        return y / x;
+    }
+
+    public int sumAllOperation(int y) {
+        return multiply(y) + sum(y) + minus(y) + divide(y);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = Calculator.sum(10);
+        System.out.println(result);
+        calculator.divide(10);
+        calculator.sumAllOperation(10);
+        result = Calculator.minus(10);
+        result = Calculator.multiply(10);
+    }
+}
---
+++ src/test/java/ru/job4j/oop/CalculatorTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private int expected = 0;
+    private int result = 0;
+    private Calculator calculator =  new Calculator();
+
+    @Test
+    public void whenSumThen15() {
+        result = Calculator.sum(10);
+        expected = 15;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMinusThen5() {
+        result = Calculator.minus(10);
+        expected = 5;
+        Assert.assertEquals(result, expected);
+
+    }
+
+    @Test
+    public void whenDivideThen2() {
+        result = calculator.divide(10);
+        expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMultiplyThen50() {
+        result = Calculator.multiply(10);
+        expected = 50;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenAllOperationsThen72() {
+        result = calculator.sumAllOperation(10);
+        expected = 72;
+        Assert.assertEquals(result, expected);
+    }
+}
+
+
