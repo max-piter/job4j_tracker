ce6875b1af940f5fbacdf40225002afbdd2a3bc8
max-piter <61771015+max-piter@users.noreply.github.com>
2022-04-18 13:24:08 +0300

    1. Эксперименты с различными GC [#1590]

---
+++ src/main/java/ru/job4j/tracker/GCTracker.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+public class GCTracker {
+    public static void main(String[] args) {
+        double start;
+        double finish;
+        MemTracker mt = new MemTracker();
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            mt.add(new Item("Item" + i));
+        }
+        finish = System.currentTimeMillis();
+        System.out.printf("%nApp running time: %f%n%n", (finish - start) / 1000);
+    }
+}
