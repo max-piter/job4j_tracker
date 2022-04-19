9a2182b8e31ab8ec6b93add6e3297fce44c2b654
max-piter <61771015+max-piter@users.noreply.github.com>
2022-04-19 19:10:20 +0300

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/GCTracker.java
+++ src/main/java/ru/job4j/tracker/GCTracker.java
@@ -1,15 +1,30 @@
 package ru.job4j.tracker;
 
 public class GCTracker {
+
+    public static void addItem(MemTracker tracker) {
+        for (int i = 0; i < 1_000_000; i++) {
+            tracker.add(new Item("Item" + i, i));
+        }
+    }
+
+    public static void deleteItem(MemTracker tracker) {
+        for (int i = 0; i < 1_000_000; i++) {
+            tracker.delete(i);
+        }
+    }
+
     public static void main(String[] args) {
+        MemTracker mt = new MemTracker();
         double start;
         double finish;
-        MemTracker mt = new MemTracker();
         start = System.currentTimeMillis();
-        for (int i = 0; i < 1_000_000; i++) {
-            mt.add(new Item("Item" + i));
-        }
+        addItem(mt);
         finish = System.currentTimeMillis();
-        System.out.printf("%nApp running time: %f%n%n", (finish - start) / 1000);
+        System.out.printf("%nApp addItem running time: %f%n%n", (finish - start) / 1000);
+         start = System.currentTimeMillis();
+         deleteItem(mt);
+         finish = System.currentTimeMillis();
+        System.out.printf("%nApp deleteItem running time: %f%n%n", (finish - start) / 1000);
     }
 }
